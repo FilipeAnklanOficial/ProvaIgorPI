1e717264f709da42f3a4db0bf86d63d226681e29
FilipeAnklanOficial <101846028+FilipeAnklan@users.noreply.github.com>
2022-09-09 20:34:20 -0300

    alteracao de pasta

--- prova_igor/src/ex1.java
+++ prova_igor/src/ex1.java
@@ -1,28 +1,23 @@
 import java.util.Scanner;
 
-public class Ex2 {
+public class ex1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int numMaior = 0;
         int numMenor = 0;
         int n1 = 1;
         int[] lerNum;
-        int soma = 0;
-        int cont = 1;
 
 
         lerNum = new int[5];
 
         for(n1 = 0; n1 < 5; n1++) {
 
-            System.out.println("Digite o "+ cont +"° número: ");
+            System.out.println("Digite o "+(n1 +1)+"° número: ");
             lerNum[n1] = in.nextInt();
 
             numMaior = lerNum[n1];
             numMenor = lerNum[n1];
-            cont++;
-
-            soma = soma + lerNum[n1];
         }
         for(n1 = 0; n1 < 5; n1++) {
             if (lerNum[n1] > numMaior) {
@@ -30,12 +25,10 @@ public class Ex2 {
             } else if (lerNum[n1] < numMenor) {
                 numMenor = lerNum[n1];
             }
-            System.out.println("Valores: "+lerNum[n1]);
         }
 
+        System.out.println("O maior número dos 5 digitados é: "+numMaior);
         System.out.println();
-        System.out.println("O maior número é: "+numMaior);
-        System.out.println("O menor número é: "+numMenor);
-        System.out.println("Média: "+(soma/n1));
+        System.out.println("O menor número dos 5 digitados é: "+numMenor);
     }
 }
